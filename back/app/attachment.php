a70b753f6a52dc44daefd60c8e431d9d9a13da11
Peter Bismonte <mwmasterweaks@gmail.com>
2022-03-16 15:40:44 +0800

    Attachment

--- back/app/attachment.php
+++ back/app/attachment.php
@@ -3,8 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\leave;
+use App\missing_time_log;
+use App\over_time;
 
 class attachment extends Model
 {
-    //
+    protected $fillable = [
+        'reference_no','filename'
+
+    ];
+
+    public function Leave()
+    {
+        return $this->hasOne(leave::class,'reference_no');
+    }
+
+    public function missing_time_log()
+    {
+        return $this->hasOne(missing_time_log::class,'reference_no');
+    }
+
+    public function over_time()
+    {
+        return $this->hasOne(over_time::class,'reference_no');
+    }
 }
